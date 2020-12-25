b5b7b72dd3117c5acd7955dbfd1bb85c38a9e718
Raymond <raymond@caromble.com>
2020-12-25 23:46:37 +0100

    Day 25

--- src/main/kotlin/Day25.kt
+++ src/main/kotlin/Day25.kt
@@ -1,7 +1,34 @@
-fun day25Part1(input: List<String>): Long {
-    return -1L
+fun day25Part1(input: String): Long {
+    val (doorPublicKey, cardPublicKey) = input.split("\n").map { it.toLong() }
+    val doorLoopSize = determineLoopSize(doorPublicKey)
+    val cardLoopSize = determineLoopSize(cardPublicKey)
+    val encryptionKey1 = transform(doorPublicKey, cardLoopSize)
+    val encryptionKey2 = transform(cardPublicKey, doorLoopSize)
+    if (encryptionKey1 != encryptionKey2) {
+        error("Encryption keys should match")
+    }
+    return encryptionKey1
 }
 
-fun day25Part2(input: List<String>): Long {
-    return -1L
+fun determineLoopSize(publicKey: Long, subjectNumber: Long = 7L): Long {
+    var value = 1L
+    var loopSize = 1L
+    while (loopSize < Integer.MAX_VALUE) {
+        value *= subjectNumber
+        value %= 20201227
+        if (value == publicKey) return loopSize
+        loopSize++
+    }
+    error("Could not determine loop size")
+}
+
+fun transform(subjectNumber: Long, loopSize: Long): Long {
+    var value = 1L
+    var i = 0L
+    while (i < loopSize) {
+        value *= subjectNumber
+        value %= 20201227
+        i++
+    }
+    return value
 }
--- src/test/kotlin/Day25KtTest.kt
+++ src/test/kotlin/Day25KtTest.kt
@@ -4,16 +4,30 @@ import org.junit.jupiter.api.Test
 internal class Day25KtTest {
 
     @Test
-    fun testDay25Part1() {
-        val input = ""
-        val values = input.split("\n")
-        assertEquals(-1L, day25Part1(values))
+    fun testCardPublicKey() {
+        val cardPublicKey = 5764801L
+        assertEquals(8L, determineLoopSize(cardPublicKey, 7L))
     }
 
     @Test
-    fun testDay25Part2() {
-        val input = ""
-        val values = input.split("\n")
-        assertEquals(-1L, day25Part2(values))
+    fun testDoorPublicKey() {
+        val doorPublicKey = 17807724L
+        assertEquals(11L, determineLoopSize(doorPublicKey, 7L))
+    }
+
+    @Test
+    fun testTransform() {
+        val cardPublicKey = 5764801L
+        val cardLoopSize = 8L
+        val doorPublicKey = 17807724L
+        val doorLoopSize = 11L
+        assertEquals(14897079L, transform(doorPublicKey, cardLoopSize))
+        assertEquals(14897079L, transform(cardPublicKey, doorLoopSize))
+    }
+
+    @Test
+    fun testDay25Part1() {
+        val input = "5764801\n17807724"
+        assertEquals(14897079L, day25Part1(input))
     }
 }
