f086ce6142eb09d215d75a614b7625dad2cfb1f0
Raymond <raymond@caromble.com>
2020-12-30 22:51:59 +0100

    2015 Day 5: Doesn't He Have Intern-Elves For This? - Part Two

--- src/main/kotlin/v2015/Day05.kt
+++ src/main/kotlin/v2015/Day05.kt
@@ -1,16 +1,28 @@
 package v2015
 
 fun day05Part1(input: List<String>): Long {
+    return input.filter { isNiceWithOldRules(it) }.count().toLong()
+}
+
+fun day05Part2(input: List<String>): Long {
     return input.filter { isNice(it) }.count().toLong()
 }
 
-private fun isNice(string: String): Boolean {
+/**
+ * A nice string —using old rules— is one with all of the following properties:
+ * - It contains at least three vowels (`aeiou` only), like `aei`, `xazegov`, or `aeiouaeiouaeiou`.
+ * - It contains at least one letter that appears twice in a row, like `xx`, `abcdde` (`dd`),
+ *   or `aabbccdd` (`aa`, `bb`, `cc`, or `dd`).
+ * - It does not contain the strings `ab`, `cd`, `pq`, or `xy`, even if they are part of one of the other requirements.
+ */
+private fun isNiceWithOldRules(string: String): Boolean {
     var vowelCount = 0L
     var hasRepeatingChar = false
     for (i in string.indices) {
         val char = string[i]
         if (i + 1 < string.length) {
             val nextChar = string[i + 1]
+
             when (char) {
                 'a' -> if (nextChar == 'b') return false
                 'c' -> if (nextChar == 'd') return false
@@ -28,6 +40,39 @@ private fun isNice(string: String): Boolean {
     return vowelCount >= 3 && hasRepeatingChar
 }
 
-fun day05Part2(input: List<String>): Long {
-    return -1L
+/**
+ * A nice string —using new rules— is one with all of the following properties:
+ * - It contains a pair of any two letters that appears at least twice in the string without overlapping,
+ *   like `xyxy` (`xy`) or `aabcdefgaa` (`aa`), but not like `aaa` (`aa`, but it overlaps).
+ * - It contains at least one letter which repeats with exactly one letter between them,
+ *   like `xyx`, `abcdefeghi` (`efe`), or even `aaa`.
+ */
+private fun isNice(string: String): Boolean {
+    val pairsOfLetters = mutableMapOf<String, Int>()
+    var hasRepeatingPair = false
+    var hasRepeatingLetterWithOneLetterBetween = false
+    for (i in string.indices) {
+        val char = string[i]
+        if (i + 1 < string.length) {
+            if (!hasRepeatingPair) {
+                val pair = "" + char + string[i + 1]
+                if (pairsOfLetters.containsKey(pair)) {
+                    if (pairsOfLetters.getValue(pair) != i - 1) {
+                        hasRepeatingPair = true
+                    }
+                } else {
+                    pairsOfLetters[pair] = i
+                }
+            }
+            if (!hasRepeatingLetterWithOneLetterBetween) {
+                if (i + 2 < string.length) {
+                    hasRepeatingLetterWithOneLetterBetween = string[i] == string[i + 2]
+                }
+            }
+        }
+        if (hasRepeatingPair && hasRepeatingLetterWithOneLetterBetween) {
+            return true
+        }
+    }
+    return false
 }
--- src/test/kotlin/v2015/Day05KtTest.kt
+++ src/test/kotlin/v2015/Day05KtTest.kt
@@ -17,4 +17,17 @@ internal class Day05KtTest {
     fun testDay05Part1(input: String, expected: Long) {
         assertEquals(expected, day05Part1(listOf(input)))
     }
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` is {1} (1=nice; 0=naughty)")
+    @CsvSource(
+        "qjhvhtzxzqqjkmpb, 1",
+        "xxyxx, 1",
+        "uurcxstgmygtbstg, 0",
+        "ieodomkazucvgmuy, 0",
+        "aaa, 0",
+        "aaaa, 1"
+    )
+    fun testDay05Part2(input: String, expected: Long) {
+        assertEquals(expected, day05Part2(listOf(input)))
+    }
 }
