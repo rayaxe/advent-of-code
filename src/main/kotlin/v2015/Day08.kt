617586325f522561006597ffaac1f935b25e6c10
Raymond <raymond@caromble.com>
2021-01-01 13:05:04 +0100

    2015 Day 8: Matchsticks

--- src/main/kotlin/v2015/Day08.kt
+++ src/main/kotlin/v2015/Day08.kt
@@ -1,9 +1,28 @@
 package v2015
 
-fun day08Part1(input: List<String>): Long {
-    return -1L
-}
+class Day08 {
 
-fun day08Part2(input: List<String>): Long {
-    return -1L
+    companion object {
+        private val asciiCharRegex = """(\\x[a-f0-9]{2})""".toRegex()
+
+        fun part1(list: List<String>): Long {
+            return list.map {
+                it.length.toLong() - it.drop(1).dropLast(1)
+                    .replace(asciiCharRegex, "1")
+                    .replace("\\\\", "1")
+                    .replace("\\\"", "1")
+                    .length
+            }.sum()
+        }
+
+        fun part2(list: List<String>): Long {
+            val surroundingDoubleQuotes = 4L
+            return list.map {
+                it.replace(asciiCharRegex, "12345")
+                    .replace("\\\\", "1234")
+                    .replace("\\\"", "1234")
+                    .length + surroundingDoubleQuotes - it.length.toLong()
+            }.sum()
+        }
+    }
 }
---
+++ src/test/kotlin/v2015/Day08KtTest.kt
@@ -0,0 +1,19 @@
+package v2015
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day08KtTest {
+
+    private val list = listOf("\"\"", "\"abc\"", "\"aaa\\\"aaa\"", "\"\\x27\"")
+
+    @Test
+    fun testDay08Part1() {
+        assertEquals(12L, Day08.part1(list))
+    }
+
+    @Test
+    fun testDay08Part2() {
+        assertEquals(19L, Day08.part2(list))
+    }
+}
