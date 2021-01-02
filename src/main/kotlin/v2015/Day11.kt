0a9022bf06b36f88b317bc4487a10ee213cde2b9
Raymond <raymond@caromble.com>
2021-01-02 17:39:42 +0100

    2015 Day 11: Corporate Policy

--- src/main/kotlin/v2015/Day11.kt
+++ src/main/kotlin/v2015/Day11.kt
@@ -1,9 +1,75 @@
 package v2015
 
-fun day11Part1(input: List<String>): Long {
-    return -1L
-}
+class Day11 {
+    companion object {
+        fun part1(input: String): String {
+            var password = input
+            var isValid = false
+            while (!isValid) {
+                password = nextPassword(password)
+                password = removeInvalidLetters(password)
+                isValid = containsThreeStraightLetters(password) && containsNonOverlappingPairsOfLetters(password)
+            }
+            return password
+        }
 
-fun day11Part2(input: List<String>): Long {
-    return -1L
+        fun part2(input: String): String {
+            return part1(part1(input))
+        }
+
+        fun containsNonOverlappingPairsOfLetters(password: String): Boolean {
+            var i = 0
+            var count = 0
+            while (i < password.length - 1) {
+                if (password[i] == password[i + 1]) {
+                    count++
+                    if (count == 2) {
+                        return true
+                    }
+                    i++
+                }
+                i++
+            }
+            return false
+        }
+
+        fun containsThreeStraightLetters(password: String): Boolean {
+            for (i in 0 until password.length - 2) {
+                val first = password[i]
+                val second = password[i + 1] - 1
+                val third = password[i + 2] - 2
+                if (first == second && second == third) {
+                    return true
+                }
+            }
+            return false
+        }
+
+        fun removeInvalidLetters(password: String): String {
+            val next = StringBuilder(password)
+            for (i in next.indices) {
+                if (next[i] == 'i' || next[i] == 'o' || next[i] == 'l') {
+                    next[i] = next[i] + 1
+                    for (j in i + 1 until next.length) {
+                        next[j] = 'a'
+                    }
+                    break
+                }
+            }
+            return next.toString()
+        }
+
+        fun nextPassword(password: String): String {
+            val next = StringBuilder(password).reverse()
+            for (i in next.indices) {
+                if (next[i] == 'z') {
+                    next[i] = 'a'
+                } else {
+                    next[i] = next[i] + 1
+                    break
+                }
+            }
+            return next.reverse().toString()
+        }
+    }
 }
---
+++ src/test/kotlin/v2015/Day11KtTest.kt
@@ -0,0 +1,56 @@
+package v2015
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.params.ParameterizedTest
+import org.junit.jupiter.params.provider.CsvSource
+
+internal class Day11KtTest {
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` becomes `{1}`")
+    @CsvSource(
+        "abcdefgh, abcdffaa",
+        "ghijklmn, ghjaabcc",
+        "iaaaaaaa, jaaaaabc"
+    )
+    fun testDay11Part1(input: String, expected: String) {
+        assertEquals(expected, Day11.part1(input))
+    }
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` contains non-overlapping pairs of letters `{1}`")
+    @CsvSource(
+        "aaaaaaaa, true",
+        "aaabcdef, false",
+        "aabbdefg, true",
+        "aabcdezz, true",
+    )
+    fun testDay11ContainsNonOverlappingPairsOfLetters(input: String, expected: Boolean) {
+        assertEquals(expected, Day11.containsNonOverlappingPairsOfLetters(input))
+    }
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` contains three straight letters `{1}`")
+    @CsvSource(
+        "aaaaaaaa, false",
+        "abcaaaaa, true"
+    )
+    fun testDay11ContainsThreeStraightLetters(input: String, expected: Boolean) {
+        assertEquals(expected, Day11.containsThreeStraightLetters(input))
+    }
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` contains valid letters `{1}`")
+    @CsvSource(
+        "aaaaaaai, aaaaaaaj",
+        "iaaaaaaa, jaaaaaaa"
+    )
+    fun testDay11RemoveInvalidLetters(input: String, expected: String) {
+        assertEquals(expected, Day11.removeInvalidLetters(input))
+    }
+
+    @ParameterizedTest(name = "Example #{index} - `{0}` has next password `{1}`")
+    @CsvSource(
+        "aaaaaaaa, aaaaaaab",
+        "azzzzzzz, baaaaaaa"
+    )
+    fun testDay11NextPassword(input: String, expected: String) {
+        assertEquals(expected, Day11.nextPassword(input))
+    }
+}
