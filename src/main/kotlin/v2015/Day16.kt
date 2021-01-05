363c0f44677147c7b072dcfb782097adf2189ff3
Raymond <raymond@caromble.com>
2021-01-05 20:30:58 +0100

    2015 Day 16: Aunt Sue

--- src/main/kotlin/v2015/Day16.kt
+++ src/main/kotlin/v2015/Day16.kt
@@ -1,9 +1,62 @@
 package v2015
 
-fun day16Part1(input: List<String>): Long {
-    return -1L
-}
+class Day16 {
+    companion object {
+        private val tickerTapeMessage = "children: 3\n" +
+                "cats: 7\n" +
+                "samoyeds: 2\n" +
+                "pomeranians: 3\n" +
+                "akitas: 0\n" +
+                "vizslas: 0\n" +
+                "goldfish: 5\n" +
+                "trees: 3\n" +
+                "cars: 2\n" +
+                "perfumes: 1"
 
-fun day16Part2(input: List<String>): Long {
-    return -1L
+        fun part1(input: List<String>): Long {
+            return matchAunt(input) { 0 }
+        }
+
+        fun part2(input: List<String>): Long {
+            return matchAunt(input) {
+                when (it) {
+                    "cats", "trees" -> 1
+                    "pomeranians", "goldfish" -> -1
+                    else -> 0
+                }
+            }
+        }
+
+        private fun matchAunt(input: List<String>, compareTo: (String) -> Int): Long {
+            val detectedCompounds = parseDetectedCompounds()
+            return input.map { parse(it) }
+                .first { (_, compounds) ->
+                    compounds.all {
+                        it.value.compareTo(detectedCompounds.getValue(it.key)) == compareTo(it.key)
+                    }
+                }.first
+        }
+
+        private fun parseDetectedCompounds() = tickerTapeMessage.split("\n").map {
+            val (compound, value) = it.split(": ")
+            compound to value.toInt()
+        }.toMap()
+
+        private val auntRegex =
+            """Sue ([0-9]+): ([a-z]+): ([0-9]+), ([a-z]+): ([0-9]+), ([a-z]+): ([0-9]+)""".toRegex()
+
+        private fun parse(text: String): Pair<Long, Map<String, Int>> {
+            val matchEntire = auntRegex.matchEntire(text)
+            return matchEntire
+                ?.destructured
+                ?.let { (number, compound1, compound1Value, compound2, compound2Value, compound3, compound3Value) ->
+                    return number.toLong() to mapOf(
+                        compound1 to compound1Value.toInt(),
+                        compound2 to compound2Value.toInt(),
+                        compound3 to compound3Value.toInt()
+                    )
+                }
+                ?: throw IllegalArgumentException("Bad input '$text'")
+        }
+    }
 }
