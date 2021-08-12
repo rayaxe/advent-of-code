2521017e78b4f701445769ac01b2c03be8aa3a54
Raymond <raymond@caromble.com>
2021-08-12 08:47:06 +0200

    2016 Day 19: An Elephant Named Joseph

--- src/main/kotlin/v2016/Day19.kt
+++ src/main/kotlin/v2016/Day19.kt
@@ -1,13 +1,58 @@
 package v2016
 
 class Day19 {
+    private class Elf(val number: Int) {
+        lateinit var nextElf: Elf
+        var numberOfPresents: Int = 1
+    }
+
     companion object {
-        fun part1(input: List<String>): Long {
-            return -1L
+        fun part1(input: Int): Long {
+            var elf = initElves(input)
+            while (true) {
+                val nextElf = elf.nextElf
+                elf.numberOfPresents += nextElf.numberOfPresents
+                elf.nextElf = nextElf.nextElf
+                if (elf == elf.nextElf) {
+                    return elf.number.toLong()
+                }
+                elf = elf.nextElf
+            }
         }
 
-        fun part2(input: List<String>): Long {
-            return -1L
+        fun part2(input: Int): Long {
+            val left = ArrayDeque<Int>()
+            for (i in 1 until (input / 2) + 2) {
+                left.addLast(i)
+            }
+            val right = ArrayDeque<Int>()
+            for (i in input downTo (input / 2) + 2) {
+                right.addLast(i)
+            }
+            while (right.isNotEmpty()) {
+                if (left.size > right.size) {
+                    left.removeLast()
+                } else {
+                    right.removeLast()
+                }
+                right.addFirst(left.removeFirst())
+                left.addLast(right.removeLast())
+            }
+            return left.first().toLong()
+        }
+
+        private fun initElves(input: Int): Elf {
+            var number = 1
+            val firstElf = Elf(number)
+            var elf = firstElf
+            repeat(input - 1) {
+                number++
+                val nextElf = Elf(number)
+                elf.nextElf = nextElf
+                elf = nextElf
+            }
+            elf.nextElf = firstElf
+            return firstElf
         }
     }
 }
---
+++ src/test/kotlin/v2016/Day19Test.kt
@@ -0,0 +1,17 @@
+package v2016
+
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.Test
+
+internal class Day19Test {
+
+    @Test
+    fun testDay19Part1() {
+        assertEquals(3L, Day19.part1(5))
+    }
+
+    @Test
+    fun testDay19Part2() {
+        assertEquals(2L, Day19.part2(5))
+    }
+}
