54b38190998330b22b12762f780f7dfb722a9583
Raymond <raymond@caromble.com>
2021-10-10 22:01:07 +0200

    2017 Day 18: Duet

--- src/main/kotlin/v2017/Day18.kt
+++ src/main/kotlin/v2017/Day18.kt
@@ -1,11 +1,20 @@
 package v2017
 
+import kotlinx.coroutines.cancel
+import kotlinx.coroutines.channels.Channel
+import kotlinx.coroutines.channels.Channel.Factory.UNLIMITED
+import kotlinx.coroutines.channels.ReceiveChannel
+import kotlinx.coroutines.channels.SendChannel
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
+import kotlinx.coroutines.runBlocking
+
 class Day18 {
     companion object {
         fun part1(input: List<String>): Long {
-            val registers = mutableMapOf<Char, Long>()
             val instructions = input.map { it.split(' ') }
 
+            val registers = mutableMapOf<Char, Long>()
             fun String.getValue(): Long {
                 return this.toLongOrNull() ?: registers.getValue(this.first())
             }
@@ -42,7 +51,68 @@ class Day18 {
         }
 
         fun part2(input: List<String>): Long {
-            return -1L
+            val instructions = input.map { it.split(' ') }
+            val programA = Program(0L, instructions)
+            val programB = Program(1L, instructions)
+            runBlocking {
+                val channelA = Channel<Long>(UNLIMITED)
+                val channelB = Channel<Long>(UNLIMITED)
+                val a = launch {
+                    programA.run(channelA, channelB)
+                }
+                val b = launch {
+                    programB.run(channelB, channelA)
+                }
+                launch {
+                    delay(500)
+                    while (true) {
+                        if (channelA.isEmpty && channelB.isEmpty) {
+                            a.cancel()
+                            b.cancel()
+                            cancel()
+                            return@launch
+                        }
+                    }
+                }
+            }
+            return programB.sendCount
+        }
+
+        private class Program(val p: Long, val instructions: List<List<String>>) {
+            var sendCount = 0L
+
+            suspend fun run(input: ReceiveChannel<Long>, output: SendChannel<Long>) {
+                val registers = mutableMapOf<Char, Long>()
+                registers['p'] = p
+                fun String.getValue(): Long {
+                    return this.toLongOrNull() ?: registers.getValue(this.first())
+                }
+
+                var pointer = 0L
+                while (pointer in instructions.indices) {
+                    val instruction = instructions[pointer.toInt()]
+                    val operation = instruction[0]
+                    val register = instruction[1].first()
+                    when (operation) {
+                        "snd" -> {
+                            output.trySend(instruction[1].getValue())
+                            sendCount++
+                        }
+                        "set" -> registers[register] = instruction[2].getValue()
+                        "add" -> registers[register] = registers.getOrDefault(register, 0) + instruction[2].getValue()
+                        "mul" -> registers[register] = registers.getOrDefault(register, 0) * instruction[2].getValue()
+                        "mod" -> registers[register] = registers.getOrDefault(register, 0) % instruction[2].getValue()
+                        "rcv" -> registers[register] = input.receive()
+                        "jgz" -> {
+                            if (instruction[1].getValue() > 0L) {
+                                pointer += instruction[2].getValue() - 1
+                            }
+                        }
+                        else -> error("Unsupported operation: $operation")
+                    }
+                    pointer += 1
+                }
+            }
         }
     }
 }
--- src/test/kotlin/v2017/Day18Test.kt
+++ src/test/kotlin/v2017/Day18Test.kt
@@ -4,26 +4,35 @@ import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.Test
 
 internal class Day18Test {
-    private val input = listOf(
-        "set a 1",
-        "add a 2",
-        "mul a a",
-        "mod a 5",
-        "snd a",
-        "set a 0",
-        "rcv a",
-        "jgz a -1",
-        "set a 1",
-        "jgz a -2"
-    )
 
     @Test
     fun testDay18Part1() {
+        val input = listOf(
+            "set a 1",
+            "add a 2",
+            "mul a a",
+            "mod a 5",
+            "snd a",
+            "set a 0",
+            "rcv a",
+            "jgz a -1",
+            "set a 1",
+            "jgz a -2"
+        )
         assertEquals(4L, Day18.part1(input))
     }
 
     @Test
     fun testDay18Part2() {
-        assertEquals(-1L, Day18.part2(input))
+        val input = listOf(
+            "snd 1",
+            "snd 2",
+            "snd p",
+            "rcv a",
+            "rcv b",
+            "rcv c",
+            "rcv d"
+        )
+        assertEquals(3L, Day18.part2(input))
     }
 }
