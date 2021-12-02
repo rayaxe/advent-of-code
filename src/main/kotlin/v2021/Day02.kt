71ea4526fea5ea126d87ba0e73e103e19f594147
Raymond <raymond@caromble.com>
2021-12-02 16:29:08 +0100

    2021 Day 2: Dive!

--- src/main/kotlin/v2021/Day02.kt
+++ src/main/kotlin/v2021/Day02.kt
@@ -5,35 +5,35 @@ class Day02 {
 
     companion object {
         fun part1(input: List<String>): Long {
-            return move(
+            return dive(
                 input,
-                { s: Submarine, x: Int -> s.copy(horizontal = s.horizontal + x) },
-                { s: Submarine, x: Int -> s.copy(depth = s.depth + x) },
-                { s: Submarine, x: Int -> s.copy(depth = s.depth - x) }
+                { x -> copy(horizontal = horizontal + x) },
+                { x -> copy(depth = depth + x) },
+                { x -> copy(depth = depth - x) }
             )
         }
 
         fun part2(input: List<String>): Long {
-            return move(
+            return dive(
                 input,
-                { s: Submarine, x: Int -> s.copy(horizontal = s.horizontal + x, depth = s.depth + s.aim * x) },
-                { s: Submarine, x: Int -> s.copy(aim = s.aim + x) },
-                { s: Submarine, x: Int -> s.copy(aim = s.aim - x) }
+                { x -> copy(horizontal = horizontal + x, depth = depth + aim * x) },
+                { x -> copy(aim = aim + x) },
+                { x -> copy(aim = aim - x) }
             )
         }
 
-        private fun move(
+        private fun dive(
             input: List<String>,
-            forward: (Submarine, Int) -> Submarine,
-            down: (Submarine, Int) -> Submarine,
-            up: (Submarine, Int) -> Submarine
+            forward: Submarine.(Int) -> Submarine,
+            down: Submarine.(Int) -> Submarine,
+            up: Submarine.(Int) -> Submarine
         ): Long {
             return input.fold(Submarine()) { submarine, it ->
-                val (command, units) = it.split(" ")
+                val (command, units) = it.split(" ").let { (s1, s2) -> s1 to s2.toInt() }
                 when (command) {
-                    "forward" -> forward(submarine, units.toInt())
-                    "down" -> down(submarine, units.toInt())
-                    "up" -> up(submarine, units.toInt())
+                    "forward" -> submarine.forward(units)
+                    "down" -> submarine.down(units)
+                    "up" -> submarine.up(units)
                     else -> error("Unexpected command: $command")
                 }
             }.let { it.horizontal * it.depth }
