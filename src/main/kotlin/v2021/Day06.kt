4218379f22858b1974391cfe793ac72f5e4a0ac8
Raymond <raymond@caromble.com>
2021-12-06 21:23:34 +0100

    2021 Day 6: Lanternfish

--- src/main/kotlin/v2021/Day06.kt
+++ src/main/kotlin/v2021/Day06.kt
@@ -12,21 +12,14 @@ class Day06 {
         }
 
         private fun simulate(fish: List<Int>, days: Int): Long {
-            var fishMap = fish
-                .groupBy { it }
-                .mapValues { it.value.size.toLong() }
+            var fishMap = fish.groupBy { it }.mapValues { it.value.size.toLong() }
 
             repeat(days) {
-                val nextDay = fishMap
-                    .filterKeys { it > 0 }
-                    .mapKeys { it.key - 1 }
-                    .toMutableMap()
+                val nextDay = fishMap.mapKeys { it.key - 1 }.toMutableMap()
                 val fishReproducing = fishMap.getOrDefault(0, 0)
-                if (fishReproducing > 0) {
-                    nextDay[6] = nextDay.getOrDefault(6, 0) + fishReproducing
-                    nextDay[8] = nextDay.getOrDefault(8, 0) + fishReproducing
-                }
-                fishMap = nextDay
+                nextDay[6] = nextDay.getOrDefault(6, 0) + fishReproducing
+                nextDay[8] = nextDay.getOrDefault(8, 0) + fishReproducing
+                fishMap = nextDay.filterKeys { it >= 0 }
             }
 
             return fishMap.values.sum()
