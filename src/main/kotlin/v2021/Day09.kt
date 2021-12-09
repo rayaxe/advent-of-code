c92b7eb1c7c6f5508e506e336bf40810e0f5470b
Raymond <raymond@caromble.com>
2021-12-09 13:53:16 +0100

    2021 Day 9: Smoke Basin

--- src/main/kotlin/v2021/Day09.kt
+++ src/main/kotlin/v2021/Day09.kt
@@ -5,30 +5,30 @@ class Day09 {
         private val directions = listOf(-1 to 0, 0 to 1, 1 to 0, 0 to -1)
 
         fun part1(input: List<String>): Long {
-            val heights = parse(input)
-            return findLowPoints(heights)
+            val heightmap = parse(input)
+            return findLowPoints(heightmap)
                 .sumOf { it.second + 1 }
         }
 
         fun part2(input: List<String>): Long {
-            val heights = parse(input)
-            return findLowPoints(heights)
-                .map { findBasinSize(it.first, heights) }
+            val heightmap = parse(input)
+            return findLowPoints(heightmap)
+                .map { findBasinSize(it.first, heightmap) }
                 .sortedDescending()
                 .take(3)
                 .reduce { acc, size -> acc * size }
         }
 
-        private fun findBasinSize(lowPoint: Pair<Int, Int>, heights: Array<Array<Long>>): Long {
+        private fun findBasinSize(lowPoint: Pair<Int, Int>, heightmap: Array<Array<Long>>): Long {
             val visited = mutableSetOf<Pair<Int, Int>>()
             val frontier = mutableListOf(lowPoint)
             while (frontier.isNotEmpty()) {
                 val (y, x) = frontier.removeFirst()
                 directions.forEach { (dy, dx) ->
                     val neighbour = y + dy to x + dx
-                    val (ny, nx) = neighbour
-                    val isInBasin = ny in heights.indices && nx in heights[0].indices && heights[ny][nx] != 9L
-                    if (isInBasin && neighbour !in visited) {
+                    val isWithinBasin =
+                        neighbour.within(heightmap) && heightmap[neighbour.first][neighbour.second] != 9L
+                    if (isWithinBasin && neighbour !in visited) {
                         visited.add(neighbour)
                         frontier.add(neighbour)
                     }
@@ -37,31 +37,29 @@ class Day09 {
             return visited.size.toLong()
         }
 
-        private fun findLowPoints(heights: Array<Array<Long>>): List<Pair<Pair<Int, Int>, Long>> {
-            val rangeY = heights.indices
-            val rangeX = heights[0].indices
-            return rangeY.flatMap { y ->
-                rangeX.mapNotNull { x ->
-                    val point = heights[y][x]
+        private fun findLowPoints(heightmap: Array<Array<Long>>): List<Pair<Pair<Int, Int>, Long>> {
+            return heightmap.indices.flatMap { y ->
+                heightmap[0].indices.mapNotNull { x ->
+                    val point = heightmap[y][x]
                     val isLowPoint = directions.all { (dy, dx) ->
                         val neighbour = y + dy to x + dx
-                        val (ny, nx) = neighbour
-                        if (ny in rangeY && nx in rangeX) {
-                            point < heights[ny][nx]
+                        if (neighbour.within(heightmap)) {
+                            point < heightmap[neighbour.first][neighbour.second]
                         } else {
                             true
                         }
                     }
-                    if (isLowPoint) ((y to x) to point) else null
+                    if (isLowPoint) (y to x) to point else null
                 }
             }
         }
 
-        private fun parse(input: List<String>): Array<Array<Long>> {
-            val heights = input.map { row ->
+        private fun Pair<Int, Int>.within(area: Array<Array<Long>>): Boolean =
+            this.first in area.indices && this.second in area[0].indices
+
+        private fun parse(input: List<String>): Array<Array<Long>> = input
+            .map { row ->
                 row.map { Character.getNumericValue(it).toLong() }.toTypedArray()
             }.toTypedArray()
-            return heights
-        }
     }
 }
